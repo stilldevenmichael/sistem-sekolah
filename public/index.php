77f65cd16216c90baa1bbf6f8008d2d3df5a2112
stilldevenmichael <devenmichael112@gmail.com>
2026-02-05 13:10:16 +0700

    first commit

---
+++ public/index.php
@@ -0,0 +1,15 @@
+<?php
+require_once './app/core/Router.php';
+
+use App\Core\Router;
+
+$router = new Router();
+
+// Register Routes
+$router->add('GET', '/students', 'StudentController', 'index');
+$router->add('GET', '/students/create', 'StudentController', 'create');
+$router->add('GET', '/students/{id}', 'StudentController', 'show');
+
+$router->run();
+
+?>
